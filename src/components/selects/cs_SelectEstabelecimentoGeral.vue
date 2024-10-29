a56179119846b7b29e5bd1ab11ee239a85e22e6b
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2024-10-29 16:48:55 -0300

    Update cs_SelectEstabelecimentoGeral.vue

--- src/components/selects/cs_SelectEstabelecimentoGeral.vue
+++ src/components/selects/cs_SelectEstabelecimentoGeral.vue
@@ -38,14 +38,14 @@ const emit = defineEmits<{
 const user = getUserFromLocalStorage();
 const tenant = user?.TenantId;
 
-const selectedItem = ref<string | null>(props.modelValue);
-const formattedList = ref<{ title: string, value: string }[]>([{ title: 'Carregando...', value: '' }]);
+const selectedItem = ref<string | null>('');
+const formattedList = ref<{ title: string, value: string }[]>([{ title: '', value: '' }]);
 const loading = ref(false);
 
 
 // Função para buscar as unidades federativas baseado no ID do país
 const fetch = async () => {
-    loading.value = true
+    loading.value = true;
     try {
         const header = {
             tenant_id: tenant,
@@ -54,19 +54,16 @@ const fetch = async () => {
             in_search: "",
             in_currentPage: 1,
             in_pageSize: 999
-        }
+        };
 
-        const _response = await axios.get(`${URLBase}CSR_BB100_Tabelas_LIB/rest/CS_TabelasTotalizacao/csicp_bb001_Get_List_Estabelecimento_Simples`, {
-            headers: header
-        })
-        //@ts-ignore
-        const res = _response as IGetListaEstabelecimento;
-        formattedList.value = res.csicp_bb001.map((item) => ({
-            title: item.BB001_NomeFantasia,
-            value: item.ID
+        const _response = await axios.get(`${URLBase}CSR_BB100_Tabelas_LIB/rest/CS_TabelasTotalizacao/csicp_bb001_Get_List_Estabelecimento_Simples`, { headers: header });
+        const res = _response.data as IGetListaEstabelecimento;
+
+        // Mapeamento otimizado
+        formattedList.value = res.csicp_bb001.map(({ BB001_NomeFantasia, ID }) => ({
+            title: BB001_NomeFantasia,
+            value: ID
         }));
-
-
     } catch (error) {
         //@ts-ignore
         console.error('Erro ao buscar dado:', error.response?.data || error.message);
@@ -74,6 +71,7 @@ const fetch = async () => {
         loading.value = false;
     }
 };
+
 
 watch(selectedItem, (newVal) => {
     emit('update:modelValue', newVal);
@@ -112,57 +110,7 @@ export interface PageSize {
 
 export interface csicp_bb001 {
     ID: string
-    BB001_CodigoEmpresa: number
-    BB001_RazaoSocial: string
-    BB001_Endereco: string
-    BB001_Complemento: string
-    BB001_NumResidencial: string
-    BB001_Bairro: string
-    BB001_CEP: number
-    BB001_CNPJ: number
-    BB001_InscEstadual: string
-    BB001_InscJunta: number
-    BB001_DataInscricao: string
     BB001_NomeFantasia: string
-    BB001_Telefone: string
-    BB001_Fax: string
-    BB001_SlogamEmpresa1: string
-    BB001_SlogamEmpresa2: string
-    BB001_Email: string
-    BB001_HomePage: string
-    BB001_RamoEmpresa: number
-    BB002_GrupoEmpresa: string
-    BB001_CodgClienteAux: number
-    BB001_AlmoxPadrao: number
-    BB001_AlmoxTransfer: number
-    BB001_BDDistribuida: number
-    BB001_CNAEFiscal: number
-    BB001_SUFRAMAEmp: string
-    BB001_InscMunicipal: string
-    BB001_PaisID: string
-    CidadeId: string
-    BB001_UFID: string
-    BB001_NomeOficial: string
-    BB001_CPF_Oficial: number
-    BB001_CodgCartorio: number
-    BB001_NomeSubstituto: string
-    BB001_DescricaoOficial: string
-    BB001_CapitalMunicipio: number
-    bb001_Cor1_Hexa: string
-    bb001_Cor2_Hexa: string
-    BB001_Idioma_ID: number
-    BB001_IsActive: boolean
-    BB001_Token: string
-    BB001_UsuarioSIRC: string
-    BB001_SenhaSIRC: string
-    BB001_TenantFiscal: number
-    bb001_Token_eSitef: string
-    bb001_URL_GooglePlay: string
-    bb001_URL_AppStore: string
-    bb001_CIX: string
-    BB001_Chave_APL: string
-    BB001_Aut_Token: string
-    BB001_Token_CSPIX: string
 }
 
 
