bf13f5342afe0ab737be1d10f04de90062659261
Victor Santos <victor.manoel@cs-consoft.com.br>
2024-11-11 09:26:33 -0300

    Update PopUpConsultarCNPJ
    Atualização no componente para exibir todos os dados após buscar um CNPJ e criação do evento para retornar esses dados para a tela pai.

--- src/components/popup/cs_PopUpRecuperarDados.vue
+++ src/components/popup/cs_PopUpRecuperarDados.vue
@@ -21,26 +21,52 @@
                     <p><strong>Natureza Jurídica:</strong> {{ cnpjData.codigo_natureza_juridica }}</p>
                     <p><strong>Porte:</strong> {{ cnpjData.porte }}</p>
                 </v-card-text>
+                <div class="d-flex">
+                    <v-col cols="6" class="pa-0">
+                        <v-card-text>
+                            <div>
+                                <h4>Endereço</h4>
+                                <v-divider></v-divider>
+                            </div>
+                            <p><strong>CEP:</strong> {{ cnpjData.cep }}</p>
+                            <p>
+                                <strong>Logradouro:</strong> {{ cnpjData.descricao_tipo_de_logradouro }} {{ cnpjData.logradouro }},
+                                {{ cnpjData.numero }}
+                            </p>
+                            <p><strong>Complemento:</strong> {{ cnpjData.complemento }}</p>
+                            <p><strong>Bairro:</strong> {{ cnpjData.bairro }}</p>
+                            <p><strong>Município:</strong> {{ cnpjData.municipio }}</p>
+                            <p><strong>UF:</strong> {{ cnpjData.uf }}</p>
+                        </v-card-text>
+                    </v-col>
+                    <v-col cols="6" class="pa-0">
+                        <v-card-text>
+                            <div>
+                                <h4>Contato</h4>
+                                <v-divider></v-divider>
+                            </div>
+                            <p><strong>Telefone 1:</strong> {{ cnpjData.ddd_telefone_1 || 'Não informado' }}</p>
+                            <p><strong>Telefone 2:</strong> {{ cnpjData.ddd_telefone_2 || 'Não informado' }}</p>
+                            <p><strong>Email:</strong> {{ cnpjData.email || 'Não informado' }}</p>
+                        </v-card-text>
+                    </v-col>
+                </div>
                 <v-card-text>
                     <div>
-                        <h4>Endereço</h4>
+                        <h4>Detalhes Empresariais</h4>
                         <v-divider></v-divider>
                     </div>
-                    <p><strong>CEP:</strong> {{ cnpjData.cep }}</p>
-                    <p>
-                        <strong>Logradouro:</strong> {{ cnpjData.descricao_tipo_de_logradouro }} {{ cnpjData.logradouro }},
-                        {{ cnpjData.numero }}
-                    </p>
-                    <p><strong>Complemento:</strong> {{ cnpjData.complemento }}</p>
-                    <p><strong>Bairro:</strong> {{ cnpjData.bairro }}</p>
-                    <p><strong>Município:</strong> {{ cnpjData.municipio }}</p>
-                    <p><strong>UF:</strong> {{ cnpjData.uf }}</p>
+                    <p><strong>CNAE Fiscal:</strong> {{ cnpjData.cnae_fiscal }} - {{ cnpjData.cnae_fiscal_descricao }}</p>
+                    <p><strong>Capital Social:</strong> R$ {{ cnpjData.capital_social.toLocaleString('pt-BR') }}</p>
+                    <p><strong>Opção pelo Simples:</strong> {{ cnpjData.opcao_pelo_simples ? 'Sim' : 'Não' }}</p>
+                    <p><strong>Opção pelo MEI:</strong> {{ cnpjData.opcao_pelo_mei ? 'Sim' : 'Não' }}</p>
                 </v-card-text>
             </div>
 
             <template v-slot:actions>
                 <v-spacer></v-spacer>
-                <cs_BtnSalvar />
+
+                <v-btn prepend-icon="mdi-share" color="success" variant="flat" @click="emitCnpjData"> Enviar </v-btn>
                 <cs_BtnCancelar @click="closeDialogFator" />
             </template>
         </v-card>
@@ -50,7 +76,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import cs_InputCnpj from '../campos/cs_InputCnpj.vue';
-import cs_BtnSalvar from '../botoes/cs_BtnSalvar.vue';
 import cs_BtnCancelar from '../botoes/cs_BtnCancelar.vue';
 
 const dialogDados = ref(false);
@@ -81,6 +106,8 @@ const cnpjData = ref({
     codigo_municipio: 0,
     municipio: '',
     ddd_telefone_1: '',
+    ddd_telefone_2: '',
+    email: '',
     qualificacao_do_responsavel: 0,
     capital_social: 0,
     porte: 0,
@@ -89,10 +116,11 @@ const cnpjData = ref({
     opcao_pelo_mei: false
 });
 
-// Função para lidar com os dados emitidos do CNPJ
+// Define o emit
+const emit = defineEmits(['enviarDadosCnpj']);
+
 function handleCnpjData(data: any) {
     cnpjData.value = data;
-    console.log(data);
 }
 
 function openDialogFator() {
@@ -129,6 +157,8 @@ function resetCnpjData() {
         codigo_municipio: 0,
         municipio: '',
         ddd_telefone_1: '',
+        ddd_telefone_2: '',
+        email: '',
         qualificacao_do_responsavel: 0,
         capital_social: 0,
         porte: 0,
@@ -136,6 +166,10 @@ function resetCnpjData() {
         opcao_pelo_simples: false,
         opcao_pelo_mei: false
     };
+}
+
+function emitCnpjData() {
+    emit('enviarDadosCnpj', cnpjData.value);
 }
 
 defineExpose({
